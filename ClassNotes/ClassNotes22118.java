9bd0f0b120577017b1911475487e84cf6a88366a
Atlask <reimorales@outlook.com>
2018-02-26 20:26:10 -0500

    Merge remote-tracking branch 'origin/Dev' into Dev
    Signed-off-by: Atlask <reimorales@outlook.com>

---
+++ ClassNotes/ClassNotes22118.java
@@ -0,0 +1,14 @@
+package ClassNotes;
+
+public class ClassNotes22118
+{
+
+
+/*
+Interfaces
+Interface Comparables
+    Provide method for "compare to"
+    Interfaces have angle brackets < >
+
+ */
+}
---
+++ Extra_Credit/Extra_Credit_One/PayrollSystem_Phase1/PayrollSystem_Phase2.java
@@ -0,0 +1,6 @@
+package Extra_Credit.Extra_Credit_One.PayrollSystem_Phase1;
+
+public class PayrollSystem_Phase2
+{
+
+}
